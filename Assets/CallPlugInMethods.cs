b37dfa07b096d6f0eff6ede5a748b5e27e4925bb
Eli G <>
2024-03-21 16:54:27 -0700

    Initial Project

---
+++ Assets/CallPlugInMethods.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CallPlugInMethods : MonoBehaviour
+{
+
+    [SerializeField] private Text text;
+private int notZero = 1;
+
+    public void PushButton(){
+        text.text = "" + notZero++;
+    }
+}
